3f8f2ea1faa51c04dc692634a50743137c4afd82
MahbubulHuq2000 <132471897+MahbubulHuq2000@users.noreply.github.com>
2023-05-03 20:53:03 +0600

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+def create_app():
+    app = Flask(__name__)
+
+    @app.route("/")
+    def bangladesh():
+        return render_template("bd.html")
+
+    @app.route("/<place>/")
+    def bandarbans(place):
+        template = "place.html"
+        return render_template(template, place= place)
+
+
+
+    if __name__ == "__main__":
+        app.run()
+    
+    return app
